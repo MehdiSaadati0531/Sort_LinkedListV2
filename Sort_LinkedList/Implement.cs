8dd12e847cd008ebc6560f18ce58f72ec61b042f
MehdiSaadati <108065298+MahdiSaadatiOrg@users.noreply.github.com>
2022-08-21 10:37:48 +0430

    Add files via upload

---
+++ Sort_LinkedList/Implement.cs
@@ -0,0 +1,77 @@
+﻿namespace Sort_LinkedList
+{
+    public class Node
+    {
+        public int Value;
+        public Node Next;
+    }
+
+    public class NodeOperation
+    {
+        Node head = null;
+
+        private bool IsNull()
+        {
+            if (head == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Insert(int key)
+        {
+            Node newNode = new Node();
+            newNode.Value = key;
+            if (IsNull())
+            {
+                head = newNode;
+                head.Next = null;
+            }
+            else if (head.Next == null && key >= head.Value)
+            {
+                head.Next = newNode;
+                newNode.Next = null;
+            }
+            else if (key <= head.Value)
+            {
+                newNode.Next = head;
+                head = newNode;
+            }
+            else
+            {
+                Node finde = head;
+
+                while (finde != null)
+                {
+                    if (finde.Next == null)
+                    {
+                        finde.Next = newNode;
+                        newNode.Next = null;
+                        return;
+                    }
+                    else if (key >= finde.Value && key <= finde.Next.Value)
+                    {
+                        newNode.Next = finde.Next;
+                        finde.Next = newNode;
+                        return;
+                    }
+                    finde = finde.Next;
+                }
+            }
+        }
+
+        public void Print()
+        {
+            Node temp = head;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.Value);
+                temp = temp.Next;
+            }
+        }
+    }
+}
---
+++ Sort_LinkedList/Program.cs
@@ -0,0 +1,16 @@
+﻿using Sort_LinkedList;
+
+
+NodeOperation a = new NodeOperation();
+
+a.Insert(1);
+a.Insert(5);
+a.Insert(6);
+a.Insert(0);
+a.Insert(0);
+a.Insert(0);
+a.Insert(2);
+a.Insert(50);
+a.Insert(35);
+a.Insert(-200);
+a.Print();
